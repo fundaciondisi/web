250981183f37654c1bc8a64656ff3cc42a60f645
César <cesar@proyetvall-server.(none)>
2013-01-24 13:48:11 +0100

    base de datos con cuadros

--- fundacion.sql
+++ fundacion.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 3.4.10.1deb1
+-- version 3.4.5deb1
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 24-01-2013 a las 12:44:31
--- Versión del servidor: 5.5.28
--- Versión de PHP: 5.3.10-1ubuntu3.4
+-- Tiempo de generación: 24-01-2013 a las 13:47:33
+-- Versión del servidor: 5.1.61
+-- Versión de PHP: 5.3.6-13ubuntu3.6
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,6 +19,30 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `fundacion`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cuadrodinamico`
+--
+
+CREATE TABLE IF NOT EXISTS `cuadrodinamico` (
+  `nombre` varchar(100) NOT NULL,
+  `posicion` int(2) NOT NULL,
+  `texto` varchar(20000) DEFAULT NULL,
+  PRIMARY KEY (`nombre`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `cuadrodinamico`
+--
+
+INSERT INTO `cuadrodinamico` (`nombre`, `posicion`, `texto`) VALUES
+('QUE ES EFICIENCIA ENERGETICA. ', 1, 'QUE ES EFICIENCIA ENERGETICA. '),
+('CURSOS FORMATIVOS', 2, 'CURSOS FORMATIVOS'),
+('INVOLUCRATE', 3, 'INVOLUCRATE'),
+('DONACIONES ', 4, 'DONACIONES '),
+('TODOS DEBEMOS PASAR LA CERTIFICACIÓN ENERGÉTICA EN LOS EDIFICIOS', 5, 'TODOS DEBEMOS PASAR LA CERTIFICACIÓN ENERGÉTICA EN LOS EDIFICIOS');
 
 -- --------------------------------------------------------
 
@@ -38,7 +62,11 @@ CREATE TABLE IF NOT EXISTS `menu` (
 --
 
 INSERT INTO `menu` (`nombre`, `apodo`, `posicion`) VALUES
-('Formación', 'formacion', 2),
+('Blog', 'blog', 5),
+('Campos de trabajo', 'campostrabajo', 3),
+('Colabora', 'colabora', 4),
+('Contacto', 'contacto', 6),
+('Oferta formativa', 'formacion', 2),
 ('Quienes somos', 'quienessomos', 1);
 
 -- --------------------------------------------------------
@@ -60,8 +88,22 @@ CREATE TABLE IF NOT EXISTS `submenu` (
 --
 
 INSERT INTO `submenu` (`menu`, `nombre`, `apodo`, `posicion`) VALUES
-('formacion', 'submenu1', 'submenu1', 1),
-('formacion', 'submenu2', 'submenu2', 2);
+('formacion', 'Acceso aula virtual', 'aulavirtual', 2),
+('formacion', 'Actividades', 'actividades', 5),
+('campostrabajo', 'Agente de cooperación', 'agentesdecooperacion', 5),
+('campostrabajo', 'Asesoramos', 'asesoramos', 1),
+('quienessomos', 'Con quien trabajamos', 'quientrabajamos', 2),
+('campostrabajo', 'Desarrollamos proyectos', 'desarrollamosproyectos', 4),
+('colabora', 'Donaciones', 'donaciones', 2),
+('formacion', 'Formación bonificada', 'formacionbonificada', 1),
+('formacion', 'Formación online', 'formaciononline', 3),
+('formacion', 'Formación presencial', 'formacionpresencial', 4),
+('campostrabajo', 'Formamos', 'formamos', 3),
+('campostrabajo', 'Gestionamos', 'gestionamos', 2),
+('colabora', 'Involúcrate', 'involucrate', 1),
+('quienessomos', 'Quienes somos', 'quienessomos', 1),
+('quienessomos', 'Sedes', 'sedes', 3),
+('quienessomos', 'Voluntariado: conoce a nuestros voluntarios', 'voluntariado', 4);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
